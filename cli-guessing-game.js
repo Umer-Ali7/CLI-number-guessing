886cf393049768fa8e0cf755baf276b460fbb775
Umer Ali <ua472443@gmail.com>
2024-03-23 17:28:34 +0500

    Created Number Guessing Game.

---
+++ cli-guessing-game.js
@@ -0,0 +1,20 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+// // 1) computer will generate a random number
+// // 2) User input for guesing number
+// // 3) compare user input with computer generated number and show result
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "please guess a number between 1-10?:"
+    },
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("Contratulation! you guessed a right number");
+}
+else {
+    console.log("You guess wrong number");
+}
+;
